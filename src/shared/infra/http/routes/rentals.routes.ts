67bca7c7ff5c3da0775846e9293d8b73bd4b696b
Laercio Rios <laercio.rios13@hotmail.com>
2022-02-10 14:38:03 -0300

    :sparkles: Added devolution rental

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const rentalsRoutes = Router();
 
 const createRentalsController = new CreateRentalController();
+const devolutionRentalsController = new DevolutionRentalController();
 
 rentalsRoutes.post("/", ensureAuthenticated, createRentalsController.handle);
+rentalsRoutes.post(
+  "/devolution/:id",
+  ensureAuthenticated,
+  devolutionRentalsController.handle
+);
 
 export { rentalsRoutes };
