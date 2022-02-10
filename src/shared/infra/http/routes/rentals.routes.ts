9f651fa16206f98b751f49d019ecec0cf4e73756
Laercio Rios <laercio.rios13@hotmail.com>
2022-02-10 15:22:43 -0300

    :sparkles: Added list rentals by user

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -2,18 +2,25 @@ import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const rentalsRoutes = Router();
 
 const createRentalsController = new CreateRentalController();
 const devolutionRentalsController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalsRoutes.post("/", ensureAuthenticated, createRentalsController.handle);
 rentalsRoutes.post(
   "/devolution/:id",
   ensureAuthenticated,
   devolutionRentalsController.handle
+);
+rentalsRoutes.get(
+  "/user",
+  ensureAuthenticated,
+  listRentalsByUserController.handle
 );
 
 export { rentalsRoutes };
